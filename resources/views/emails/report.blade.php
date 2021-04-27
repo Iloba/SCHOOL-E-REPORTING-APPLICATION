97027e2a7f8e59b70dbb9d017b5560ecca089ba5
Iloba <scarlettsney@gmail.com>
2021-04-27 01:58:34 +0100

    tuesday midnight

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.email')
 @section('content')
 @foreach ($student as $stud)
-   <div style="font-family: 'nunito sans', sans-serif; background: rgb(243, 243, 243); padding: 15px;">
+   <div style="font-family: 'nunito sans', sans-serif; background: rgb(243, 243, 243);">
         <img style="width: 100%;" src="{{$message->embed('./img/email-head.jpg')}}" alt="E-Report">
-        <h3>Good day {{$stud->Guardian_name}}, </h3>
-        <h4>Please Find Below, The School Report of your Ward  <b style="color: rgb(104, 5, 104);">{{$stud->name}} (<small>{{$stud->class}}</small>)</b> <br> in {{$stud->school}} 
+        <div style="padding: 14px;">
+            <h3>Good day {{$stud->Guardian_name}}, </h3>
+        </div>
+        <h4 style="padding: 14px;">Please Find Below, The School Report of your Ward  <b style="color: rgb(104, 5, 104);">{{$stud->name}} (<small>{{$stud->class}}</small>)</b> <br> in {{$stud->school}} 
         <br></h4>
         <div class="row">
             <div class="col-md-6">
-                <img width="20%" src="{{$message->embed('./storage/students_passports/'.$stud->passport_photograph)}}" alt="{{$stud->name}}">
+                <img style="padding: 14px;" width="20%" src="{{$message->embed('./storage/students_passports/'.$stud->passport_photograph)}}" alt="{{$stud->name}}">
             </div>
             <div class="col-md-6">
                 <div class="card">
