1102d383192ec1f2f54ad2df2b59b6f29e106da2
Iloba <scarlettsney@gmail.com>
2021-04-27 16:23:53 +0100

    update

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -3,14 +3,14 @@
 @foreach ($student as $stud)
    <div style="font-family: 'nunito sans', sans-serif; background: rgb(243, 243, 243);">
         <img style="width: 100%;" src="{{$message->embed('./img/email-head.jpg')}}" alt="E-Report">
-        <div style="padding: 14px;">
+        <div style="padding: 10px;">
             <h3>Good day {{$stud->Guardian_name}}, </h3>
         </div>
-        <h4 style="padding: 14px;">Please Find Below, The School Report of your Ward  <b style="color: rgb(104, 5, 104);">{{$stud->name}} (<small>{{$stud->class}}</small>)</b> <br> in {{$stud->school}} 
+        <h4 style="padding: 10px;">Please Find Below, The School Report of your Ward  <b style="color: rgb(104, 5, 104);">{{$stud->name}} (<small>{{$stud->class}}</small>)</b> <br> in {{$stud->school}} 
         <br></h4>
         <div class="row">
             <div class="col-md-6">
-                <img style="padding: 14px;" width="20%" src="{{$message->embed('./storage/students_passports/'.$stud->passport_photograph)}}" alt="{{$stud->name}}">
+                <img style="padding: 10px;" width="20%" src="{{$message->embed('./storage/students_passports/'.$stud->passport_photograph)}}" alt="{{$stud->name}}">
             </div>
             <div class="col-md-6">
                 <div class="card">
